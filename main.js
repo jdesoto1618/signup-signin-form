00cc19193cd5eec0eda756a79c85978373f21a32
Joe DeSoto <modulusman@gmail.com>
2019-05-06 16:47:35 -0700

    minor update in main.js file
    adds more code formatting and consistency, actual code unchanged

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 // store dom objects
-var signUpButton = document.getElementById('signUp');
-var signInButton = document.getElementById('signIn');
-var container = document.getElementById('container');
+var signUpButton  = document.getElementById('signUp');
+var signInButton  = document.getElementById('signIn');
+var container     = document.getElementById('container');
 
 // sign up button event listener
 signUpButton.addEventListener('click', () => container.classList.add('right-panel-active')
 );
+
 // sign in button event listener
 signInButton.addEventListener('click', () => container.classList.remove('right-panel-active')
 );
