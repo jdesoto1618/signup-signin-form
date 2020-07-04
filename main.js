54316699e7ffa95dfde7cbda76b6d01446156478
Joe <modulusman@gmail.com>
2020-07-04 11:06:59 -0700

    Adds opacity and position transitioning effects to Sign In h1.

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const signUpButton = document.querySelector('#signUp');
 const signInButton = document.querySelector('#signIn');
 const container = document.querySelector('#container');
+const signInText = document.querySelector('.sign-in-text');
 
 signUpButton.addEventListener('click', () => container.classList.add('right-panel-active')
 );
 
 signInButton.addEventListener('click', () => container.classList.remove('right-panel-active')
 );
+
+window.onload = () => {
+  console.log(signInText.classList);
+  signInText.classList.add('show-sign-in-text');
+}
