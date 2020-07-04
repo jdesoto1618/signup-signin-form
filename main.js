8e1bbd6b1c3d942f9a2d8fd79672fecba56726b3
Joe <modulusman@gmail.com>
2020-07-04 11:49:40 -0700

    Adds text title transition effects on button clicks.
    The effect continues to happen each time sign up or
    sign in buttons are clicked and not just on page load.

--- main.js
+++ main.js
@@ -2,14 +2,21 @@ const signUpButton = document.querySelector('#signUp');
 const signInButton = document.querySelector('#signIn');
 const container = document.querySelector('#container');
 const signInText = document.querySelector('.sign-in-text');
+const createAccountText = document.querySelector('.create-account-text');
 
-signUpButton.addEventListener('click', () => container.classList.add('right-panel-active')
-);
+signUpButton.addEventListener('click', () => {
+  container.classList.add('right-panel-active');
+  createAccountText.classList.add('show-text');
+  signInText.classList.remove('show-text');
+});
 
-signInButton.addEventListener('click', () => container.classList.remove('right-panel-active')
-);
+signInButton.addEventListener('click', () => {
+  container.classList.remove('right-panel-active');
+  signInText.classList.add('show-text');
+  signInText.style.transitionDelay = '0.35s';
+  createAccountText.classList.remove('show-text');
+});
 
 window.onload = () => {
-  console.log(signInText.classList);
-  signInText.classList.add('show-sign-in-text');
+  signInText.classList.add('show-text');
 }
