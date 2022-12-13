d44881ee176b3fd56c9d0ad9595242f96172b4e3
Joe DeSoto <modulusman@gmail.com>
2022-12-13 11:13:25 -0800

    Updates container div class to be what is used in the JS file. This enables the somewhat redundant container ID the be removed from the same div.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const signUpButton = document.querySelector("#signUp");
 const signInButton = document.querySelector("#signIn");
 const loginButton = document.querySelector(".login-button");
-const container = document.querySelector("#container");
+const formWrapper = document.querySelector(".container");
 const signInText = document.querySelector(".form-heading.sign-in");
 const createAccountText = document.querySelector(
   ".form-heading.create-account"
@@ -9,13 +9,13 @@ const createAccountText = document.querySelector(
 let inputs = document.querySelectorAll("form input");
 
 signUpButton.addEventListener("click", () => {
-  container.classList.add("right-panel-active");
+  formWrapper.classList.add("right-panel-active");
   createAccountText.classList.add("show-text");
   signInText.classList.remove("show-text");
 });
 
 signInButton.addEventListener("click", () => {
-  container.classList.remove("right-panel-active");
+  formWrapper.classList.remove("right-panel-active");
   signInText.classList.add("show-text");
   createAccountText.classList.remove("show-text");
 });
