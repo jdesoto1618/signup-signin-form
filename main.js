4d289664c9c7720af8645a57d9a5354f3b7d0caa
Joe DeSoto <modulusman@gmail.com>
2022-12-14 11:22:02 -0800

    Removes unused JS and also refactors the duplicate JS. Adds another utility class to overlay buttons so they can both be targeted with one selector and iterated over.

--- main.js
+++ main.js
@@ -1,20 +1,10 @@
-const signUpButton = document.querySelector("#signUp");
-const signInButton = document.querySelector("#signIn");
-const loginButton = document.querySelector(".login-button");
+const formButtons = document.querySelectorAll(".overlay-button");
 const formWrapper = document.querySelector(".container");
 const formHeading = document.querySelector(".form-heading");
-const signInText = document.querySelector(".form-heading.sign-in");
-const createAccountText = document.querySelector(
-  ".form-heading.create-account"
-);
-let inputs = document.querySelectorAll("form input");
 
-signUpButton.addEventListener("click", () => {
-  formWrapper.classList.toggle("right-panel-active");
-  formHeading.classList.toggle("show-text");
-});
-
-signInButton.addEventListener("click", () => {
-  formWrapper.classList.toggle("right-panel-active");
-  formHeading.classList.toggle("show-text");
+formButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    formWrapper.classList.toggle("right-panel-active");
+    formHeading.classList.toggle("show-text");
+  });
 });
